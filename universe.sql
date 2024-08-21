f486f8557704601121f0f8e6340db94bbc7682a6
borospatri <155725673+borospatri@users.noreply.github.com>
2024-08-21 12:36:31 +0300

    Create universe.sql

---
+++ universe.sql
@@ -0,0 +1,14 @@
+camper: /project$ git push -u origin main
+Enumerating objects: 55, done.
+Counting objects: 100% (55/55), done.
+Delta compression using up to 16 threads
+Compressing objects: 100% (33/33), done.
+Writing objects: 100% (55/55), 32.31 KiB | 16.16 MiB/s, done.
+Total 55 (delta 21), reused 44 (delta 17), pack-reused 0
+remote: Resolving deltas: 100% (21/21), done.
+remote: fatal: did not receive expected object 80616d33e8437479fc376fb9a5657fa4d6e5332e
+error: remote unpack failed: index-pack failed
+To https://github.com/borospatri/_.git
+ ! [remote rejected] main -> main (failed)
+error: failed to push some refs to 'https://github.com/borospatri/_.git'
+camper: /project$ 
